ece348fd7efa38cff07e4c75dcb54d62c5fec18f
iyinraphael <iyinraphael@gmail.com>
2020-02-10 22:20:31 -0500

    Initial commit

---
+++ HappyDays/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HappyDays
+//
+//  Created by Iyin Raphael on 2/10/20.
+//  Copyright © 2020 Iyin Raphael. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
